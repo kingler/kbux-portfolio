d4b92b434557bfc8070fb84e8218e7fb98de4e8e
Kingler Bercy <719237+kingler@users.noreply.github.com>
2023-06-17 16:45:06 -0400

    Initial commit

---
+++ src/components/about/About.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classes from './About.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+
+const About: React.FC = () => {
+  return (
+    <div className={classes.box} id='about'>
+      <ScrollAnimation offset={0} animateIn='fadeInLeft' duration={2.4} animateOnce initiallyVisible>
+        <span className={classes.head}>ABOUT ME</span>
+        <h2 className={classes.heading}>Who Am I?</h2>
+        <div className={classes.About}>
+          <p>
+            My name is <b>SACHIN MITTAL</b> and I am currently working as <b>Software Developer Engineer</b> at{' '}
+            <b>
+              {' '}
+              <a className={classes.link} target='_blank' rel='noopener noreferrer' href='https://www.springworks.in/'>
+                Springworks
+              </a>
+            </b>
+            . I completed my degree in Bachelor of Technology in Computer Science and Engineering from Sant Longowal Institute
+            of Engineering and Technology. I am much interested in developing new things which excite me a lot. :)
+          </p>
+          <p className={classes.br}>
+            I love exploring new technologies and being a practitioner, I like to stay on top of latest trends. I try to leave
+            every line of code I write more readable, accessible, and modular.
+          </p>
+        </div>
+      </ScrollAnimation>
+    </div>
+  );
+};
+
+export default About;
